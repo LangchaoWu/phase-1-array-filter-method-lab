79f03ed82e227c2461320674551c465e0f103cd4
Langchao Wu <langchaowu@yahoo.com>
2022-02-27 19:30:11 -0500

    commit tree 879e6d713aac5b3d46b9ec5b9d3ddc48ea054d77

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers,name){
+    return drivers.filter(function(driver){
+        return driver.toUpperCase()===name.toUpperCase();
+    });
+}
+
+function fuzzyMatch(drivers,letter){
+    return drivers.filter(function(driver){
+        return driver.startsWith(letter);
+    });
+}
+
+function matchName(drivers,str){
+    
+    return drivers.filter((driver) => driver.name=== str);
+}
